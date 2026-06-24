8c02d94d58eb994526e5e70a2f5cd90e6dd4a61c
Jesvell-Jino-WHS <jc24190@my.westlake.school.nz>
2026-06-24 12:00:03 +1200

    as

--- PYTHON/L4.py
+++ PYTHON/L4.py
@@ -1,3 +1,11 @@
 
 nums = map(int, input().split())
 
+cons = []
+cuts = []
+
+def consecutive(cons, index):
+    if nums[index] == (cons[-1] + 1):
+        cons.append(nums)
+for i in range(len(nums) - 1):
+    nums[i]
