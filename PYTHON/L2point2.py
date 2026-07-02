2d4a873d526daa4984e8256c796e92a2900d8a85
Jesvell-Jino-WHS <jc24190@my.westlake.school.nz>
2026-07-02 12:01:04 +1200

    asdad

--- PYTHON/L2point2.py
+++ PYTHON/L2point2.py
@@ -1,1 +1,15 @@
-for i in range(5)
+a = int(input()) # lengths
+nums = list(map(int, input().split())) #ribbons
+cutnum = 0
+cuts = []
+
+for i in range(a):
+    if i == 0:
+        cuts.append([i])
+    elif nums[i] == (nums[i - 1] + 1):
+        cuts[cutnum].append(nums[i])
+    else:
+        cuts.append([i])
+        cutnum += 1
+
+print((len(cuts) - 1), len(max(cuts, key=len)))
