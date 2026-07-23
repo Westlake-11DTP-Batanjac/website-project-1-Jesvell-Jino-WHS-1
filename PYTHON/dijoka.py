5dd6e06a382f6219eed9d47490db1cfe773efd6e
Jesvell-Jino-WHS <jc24190@my.westlake.school.nz>
2026-07-23 12:06:18 +1200

    dasd

--- PYTHON/dijoka.py
+++ PYTHON/dijoka.py
@@ -1,17 +1,8 @@
-colleon = {} # for connecitons
-countleon = {} # counting
-N = int(input())
-for i in range(N):
-    line = list(map(str, input().split()))
-    operation = line[0]
-    if operation == "SET":
-            colleon[line[1]] = line[2]
-            countleon[line[2]] = int(countleon[line[2]]) + 1
-    elif operation == "COUNT":
-        print(countleon.get(line[1]))
-    elif operation == "GET":
-        print(colleon.get(line[1]))
-    elif operation == "REMOVE":
-        countleon[colleon[line[1]]] = countleon[colleon[line[1]]] - 1
-        colleon.pop(line[1])
-    
+t = int(input()) # test cases
+for i in range(t):
+    x, y = map(int, input().split())
+    z = x / y
+    if int(z) == z:
+        print("YES")
+    else:
+        print("NO")
