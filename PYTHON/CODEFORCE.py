a2c889833187cfee27aebe99ed7ee1c5ffd12df4
Jesvell-Jino-WHS <jc24190@my.westlake.school.nz>
2026-07-24 11:59:32 +1200

    ad

--- PYTHON/CODEFORCE.py
+++ PYTHON/CODEFORCE.py
@@ -1,18 +1,36 @@
 n = int(input())
-a = list(map(int, input().split()))
+a = list(map(int, input().split())) # A index is chef num, value is skill
+d = a.copy() # dish list
 copylist = [[] for i in range(n)]
 
 for i in range(n):
-    ci, *d = map(int, input().split())
-    copylist[ci] = d
+    ci, *cq = map(int, input().split()) # ci the chef chosen and cq the chefs chosen chef can copy
+    copylist[ci] = cq
 
 q = int(input())
 
 for i in range(q):
     line = list(map(int, input().split()))
     if line[0] == 1:
-        k, l, r = line[1], line[2], line[3]
-        print()
+        s = d.copy() # simulation list
+        sum = 0
+        k, l, r = line[1], line[2], line[3] # kth day
+        for i in range(k): #days
+            for x in range(l, r): # per chefs
+                chef = s[x]
+                # MORNING
+                if (a[x] * s[x]) > chef:
+                    chef = a[x] * chef
+                else:
+                    pass
+                # AFTERNOON
+                for i in a[x]:
+                    if (chef + i) > chef:
+                        chef = chef + i
+            
+
+
+        print((sum % ((10 ** 9) + 7)))
     elif line[0] == 2:
         i, x = line[1], line[2]
         print()
