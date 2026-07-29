fdac48a9b953e6e41c5ff6d6180b7d5a9974cd6c
Jesvell-JinoWHS <jc24190@my.westlake.school.nz>
2026-07-29 15:09:19 +1200

    ada

--- PYTHON/L2point2.py
+++ PYTHON/L2point2.py
@@ -1,5 +1,24 @@
-num = int(input())
-runner = ""
-for i in range(1, num + 1):
-    runner += str(i)
-    print(runner)
+t = int(input())
+n = []
+s = []
+for i in range(t):
+    n.append(int(input()))
+    s.append(input())
+
+for i in range(t):
+    silly = list(s[i])
+    while True:
+        old = i - 1
+        if len(silly) == 1:
+            break
+        else:
+            for i in range(n[i]):
+                if silly[old] == silly[i]:
+                    silly.pop(silly[old])
+                    silly.pop(silly[i])
+                    if silly[old] == "1":
+                        new_char = "0"
+                    elif silly[old] == "0":
+                        new_char = "1"
+                    silly.insert(old, new_char)
+                old = silly[i]
