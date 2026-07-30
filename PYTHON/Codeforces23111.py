07f723b724440cc7b72b16134c940a797b272105
Jesvell-JinoWHS <jc24190@my.westlake.school.nz>
2026-07-30 19:20:34 +1200

    sada

--- PYTHON/Codeforces23111.py
+++ PYTHON/Codeforces23111.py
@@ -1,22 +1,12 @@
 t = int(input())
-cards = [[[] for i in range(t)] for i in range(t)]
 for i in range(t):
     k = int(input())
-    cards = map(int, input().split()) # a b c ...
+    cards = list(map(int, input().split())) # a b c ...
     higherthantwo = 0
-    passed = False
+    passed = "NO"
     for i in cards:
-        if higherthantwo >= 2:
-            print("YES")
-            passed = True
+        if k == 1 and i >= 3:
+            passed = "YES"
             break
-        elif i > 2:
-            print("YES")
-            passed = True
-            break
-        elif i >= 2:
-            higherthantwo += 1
-    if higherthantwo >= 2:
-        print("YES")
-    elif passed == False:
-        print("NO")
+        elif 
+    print(passed)
