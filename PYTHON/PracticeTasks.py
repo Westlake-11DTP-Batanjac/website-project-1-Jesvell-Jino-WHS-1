3affd35d2c43bc0ccae77d748516b91ada091be5
Jesvell-JinoWHS <jc24190@my.westlake.school.nz>
2026-08-12 10:02:54 +1200

    ad

--- PYTHON/PracticeTasks.py
+++ PYTHON/PracticeTasks.py
@@ -1,18 +1,10 @@
-student_names = [] # list of student names
+student_names = ["Alice", "Ben", "Charlie", "Diana", "Ethan"] # list of student names
 student_scores = {} # dictionary to store scores
 # a space
-number_of_students = int(input("How many students do you have?: ")) #find how many studdent
-for students in range(number_of_students): # for each student ask their name and score
-    name = input("What is the student " + str(students) + " ") # ask for the name
-    while True:
-        try:
-            score = int(input("What is the score for that student: "))
-        except:
-            print("Not a valid integer, do it again.")
-        else:
-            break
 
-for students in range(number_of_students):
+for names in range(len(student_names)):
+    score = int(input("What is the score for " + names + "?: "))
 
+for names in range(student_names):
     if score >= 50:
-        print(students)
+        print(names)
