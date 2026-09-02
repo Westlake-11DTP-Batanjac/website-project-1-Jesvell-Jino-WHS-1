2f7777975649f1c5d2fd0c25ea3c32d78c53000a
Jesvell-JinoWHS <jc24190@my.westlake.school.nz>
2026-09-02 13:55:19 +1200

    da

--- FLASK/app.py
+++ FLASK/app.py
@@ -1,14 +1,1 @@
-from flask import Flask 
-
-app = Flask(__name__) 
-
-@app.route("/") 
-def home(): 
-    return "<h1>Jesvell Jino</h1>" 
-
-@app.route("/hobbies") 
-def hobbies(): 
-    return "<ul><li>Gaming<li>Eating<li>Coding<ul>"
-
-if __name__ == "__main__": 
-    app.run(debug=True)
+from flask import Flask
