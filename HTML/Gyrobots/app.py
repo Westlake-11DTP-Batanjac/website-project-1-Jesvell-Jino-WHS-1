8ee621d0baa3f160ab496b4816df9a53b5718b7a
Jesvell-JinoWHS <jc24190@my.westlake.school.nz>
2026-09-02 14:20:32 +1200

    adad

--- HTML/Gyrobots/app.py
+++ HTML/Gyrobots/app.py
@@ -3,22 +3,20 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
     return render_template("GYROBOTS.html")
 
-@app.route("/GYROBOTS.html")
-def home2():
-    return render_template("GYROBOTS.html")
-
-@app.route("/about.html")
+@app.route("/about")
 def about():
     return render_template("about.html")
 
-@app.route("/cvdl.html")
+
+@app.route("/cvdl")
 def cvdl():
     return render_template("cvdl.html")
 
-@app.route("/mork.html")
+
+@app.route("/mork")
 def mork():
     return render_template("mork.html")
 
@@ -30,7 +28,11 @@ def signin():
     print("First name:", fname)
     print("Last name:", lname)
 
-    return render_template("base.html", fname=fname, lname=lname)
+    return render_template(
+        "base.html",
+        fname=fname,
+        lname=lname
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
